db277b794803f86b46b98aaaa27a57f271270862
manishgoyaler <manishgoyaler@gmail.com>
2016-01-27 00:11:21 -0500

    updating docker file

--- rundockerCommend.sh
+++ rundockerCommend.sh
@@ -1,5 +1,6 @@
+docker commit -m "Added devbox image" -a "Manish goyal" ba57be8a2b16 manishgoyaler/devbox2
+docker push manishgoyaler/devbox
 mkdir -p ~/devbox/cassandra
 mkdir -p ~/devbox/nginx/html
 docker run -it --link dockerfile_devbox-cassandra_1:cassandra --rm cassandra sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR"'
-
-docker run -it --link dockerfile_devbox-docker_1:dockerfile_devbox-docker --rm dockerfile_devbox-docker sh 
+docker run -it --link dockerfile_devbox-docker_1:dockerfile_devbox-docker --rm dockerfile_devbox-docker /bin/bash
