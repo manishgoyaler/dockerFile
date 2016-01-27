eff7a7fc7e88943e464f52371d4fb602867d9393
manishgoyaler <manishgoyaler@gmail.com>
2016-01-26 23:37:39 -0500

    updating files to fix dev env issue

--- rundockerCommend.sh
+++ rundockerCommend.sh
@@ -1,6 +1,5 @@
-#docker pull jdubois/devbox-docker
-#docker run --name devbox -w /home/devbox/workspace -v ~/devbox:/home/devbox/workspace:rw -v ~/.m2:/home/devbox/.m2:rw -p 8080:8080 -p 3000:3000 -p 3001:3001 -d -t jdubois/devbox-docker
 mkdir -p ~/devbox/cassandra
 mkdir -p ~/devbox/nginx/html
-docker run --name devbox-cassandra -v ~/devbox/cassandra:/var/lib/cassandra -d cassandra:latest
-docker run -it --link devbox-cassandra:cassandra --rm cassandra sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR"
+docker run -it --link dockerfile_devbox-cassandra_1:cassandra --rm cassandra sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR"'
+
+docker run -it --link dockerfile_devbox-docker_1:dockerfile_devbox-docker --rm dockerfile_devbox-docker sh 
