def63448c373f31b2d6b513b19120bebb1a04a58
manishgoyaler <manishgoyaler@gmail.com>
2016-01-27 00:47:09 -0500

    adding instruction to exit docker shell

--- rundockerCommend.sh
+++ rundockerCommend.sh
@@ -5,3 +5,4 @@ mkdir -p ~/devbox/cassandra
 mkdir -p ~/devbox/nginx/html
 docker run -it --link dockerfile_devbox-cassandra_1:cassandra --rm cassandra sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR"'
 docker run -it --link dockerfile_devbox-docker_1:dockerfile_devbox-docker --rm dockerfile_devbox-docker /bin/bash
+#To exit bash shell "exit" command or enter Ctrl-D
